a48ee137ab0979eb498a44b7b7febfa415d67465
Marcin Antas <marcin.antas@herolynx.com>
2021-04-14 14:24:57 +0200

    gh-8 Implemented handling of edge cases

--- qna.py
+++ qna.py
@@ -8,7 +8,6 @@ class AnswersInput(BaseModel):
     text: str
     question: str
 
-
 class Qna:
     model: AutoModelForQuestionAnswering
     tokenizer: AutoTokenizer
@@ -25,24 +24,22 @@ class Qna:
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-    # TODO: what do we need to do to support CUDA?
-    async def do(self, input: AnswersInput):
-        inputs = self.tokenizer.encode_plus(input.question, input.text,
-                add_special_tokens=True, return_tensors="pt")
-        input_ids = inputs["input_ids"].tolist()[0]
+    def tryToAnswer(self, inputs_question, inputs_text):
+        input_ids_question = inputs_question["input_ids"].tolist()[0]
+        input_ids_text = inputs_text["input_ids"].tolist()[0]
 
+        input_ids = input_ids_question + input_ids_text
 
-        text_tokens = self.tokenizer.convert_ids_to_tokens(input_ids)
+        inputs = {
+            'input_ids': torch.cat((inputs_question['input_ids'], inputs_text['input_ids']), 1),
+            'token_type_ids': torch.cat((inputs_question['token_type_ids'], inputs_text['token_type_ids']), 1),
+            'attention_mask': torch.cat((inputs_question['attention_mask'], inputs_text['attention_mask']), 1),
+        }
+
         outputs = self.model(**inputs)
 
         answer_start_scores = outputs.start_logits
         answer_end_scores = outputs.end_logits
-
-
-        print("start")
-        print(answer_start_scores)
-        print("end")
-        print(answer_end_scores)
 
         answer_start = torch.argmax(answer_start_scores)
         start_score = torch.max(answer_start_scores)
@@ -50,8 +47,76 @@ class Qna:
         end_score = torch.max(answer_end_scores)
 
         score = (start_score + end_score) / 2
-        print("score is {}".format(score))
         ids = input_ids[answer_start:answer_end]
+        certainty = float(score) / 10
 
         return self.tokenizer.convert_tokens_to_string(
-                self.tokenizer.convert_ids_to_tokens(ids))
+                self.tokenizer.convert_ids_to_tokens(ids)), certainty
+
+    def getInputsText(self, inputs_text, window_start, window_end):
+        new_inputs_text={
+                'input_ids': torch.tensor([inputs_text['input_ids'][0][window_start:window_end].tolist() + [101]]),
+                'token_type_ids': torch.tensor([inputs_text['token_type_ids'][0][window_start:window_end].tolist() + [0]]),
+                'attention_mask': torch.tensor([inputs_text['attention_mask'][0][window_start:window_end].tolist() + [1]]),
+        }
+        return new_inputs_text
+
+    def getTokenizedInputs(self, question, text):
+        inputs_question = self.tokenizer('[CLS] ' + question + ' [SEP] ', add_special_tokens=False, 
+                return_tensors="pt")
+
+        inputs_text = self.tokenizer(text + ' [CLS]', add_special_tokens=False, 
+                return_tensors="pt")
+
+        total_length = len(inputs_question['input_ids'][0]) + len(inputs_text['input_ids'][0])
+
+        def windowSlice(arr, start, stop, block, overlap, arr_to_cut, result=[]):
+            if start == len(arr):
+                return
+            if stop > len(arr):
+                result.append(self.getInputsText(arr_to_cut, start, len(arr)))
+                return
+            else:
+                result.append(self.getInputsText(arr_to_cut, start, stop))
+            windowSlice(arr, stop-overlap, stop-overlap+block, block, overlap, arr_to_cut, result)
+
+        treshold = 512
+        windowed_input_texts=[]
+        if total_length > treshold:
+            block = treshold - len(inputs_question['input_ids'][0]) - 1
+            overlap = int(int(block) / 2)
+            windowSlice(inputs_text['input_ids'][0], 0, block, block, overlap, inputs_text, windowed_input_texts)
+        else:
+            windowed_input_texts.append(inputs_text)
+
+        result = []
+        for window_input_text in windowed_input_texts:
+            result.append((inputs_question, window_input_text))
+
+        return result
+
+    # TODO: what do we need to do to support CUDA?
+    async def do(self, input: AnswersInput):
+        question = input.question
+        text = input.text
+        if len(question) == 0 or len(text) == 0:
+            return None, None
+
+        answersScores=[]
+        inputs = self.getTokenizedInputs(question, text)
+        for input_value in inputs:
+            answersScores.append(self.tryToAnswer(input_value[0], input_value[1]))
+
+        response = ("", 0.0)
+        if len(answersScores) > 0:
+            for res in answersScores:
+                if res[1] > response[1]:
+                    response = res
+                # print("answer: ", res[0], res[1])
+
+        answer = response[0]
+        certainty = response[1]
+        if len(answer) > 0 and (answer.find("[CLS]") != -1 or answer.find("[SEP]") != -1):
+            return None, None
+
+        return answer, certainty
