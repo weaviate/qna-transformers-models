f2f1e2c601ec7c491be8bd3c5c9c73625c12f093
Etienne Dilocker <etienne@dilocker.de>
2021-04-16 11:44:26 +0200

    experiment with windows based on tokens

--- qna.py
+++ qna.py
@@ -27,12 +27,40 @@ class Qna:
 
     # TODO: what do we need to do to support CUDA?
     async def do(self, input: AnswersInput):
-        inputs = self.tokenizer.encode_plus(input.question, input.text,
-                add_special_tokens=True, return_tensors="pt")
-        input_ids = inputs["input_ids"].tolist()[0]
+
+        inputs_question = self.tokenizer('[CLS] ' + input.question + ' [SEP] ' , add_special_tokens=False, 
+                return_tensors="pt")
+
+        inputs_text = self.tokenizer(input.text + ' [CLS]', add_special_tokens=False, 
+                return_tensors="pt")
+
+        total_length = len(inputs_question['input_ids'][0]) + len(inputs_text['input_ids'][0])
+        print("total lenght is {}".format(total_length))
+
+        window_start=100
+        window_end=200
+        if total_length > 256:
+            inputs_text={
+                    'input_ids': torch.tensor([inputs_text['input_ids'][0][window_start:window_end].tolist() + [101]]),
+                    'token_type_ids': torch.tensor([inputs_text['token_type_ids'][0][window_start:window_end].tolist() + [0]]),
+                    'attention_mask': torch.tensor([inputs_text['attention_mask'][0][window_start:window_end].tolist() + [1]]),
+            }
 
 
+
+        input_ids_question = inputs_question["input_ids"].tolist()[0]
+        input_ids_text = inputs_text["input_ids"].tolist()[0]
+
+        input_ids = input_ids_question + input_ids_text
+
+
+        inputs = {
+            'input_ids': torch.cat((inputs_question['input_ids'], inputs_text['input_ids']), 1),
+            'token_type_ids': torch.cat((inputs_question['token_type_ids'], inputs_text['token_type_ids']), 1),
+            'attention_mask': torch.cat((inputs_question['attention_mask'], inputs_text['attention_mask']), 1),
+        }
         text_tokens = self.tokenizer.convert_ids_to_tokens(input_ids)
+        print(text_tokens)
         outputs = self.model(**inputs)
 
         answer_start_scores = outputs.start_logits
