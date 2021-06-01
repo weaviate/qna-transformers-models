f1add99588f0451ed23ce17100eddf6a2510d0ae
Etienne Dilocker <etienne@dilocker.de>
2021-06-01 14:44:47 +0200

    support multi-arch builds

--- cicd/docker_push.sh
+++ cicd/docker_push.sh
@@ -6,16 +6,16 @@ set -e pipefail
 # If not on main
 # - nothing is pushed
 # If on main and not PR
-# - any commit is pushed as :<model>-<7-digit-hash>
+# - any commit is pushed as :<model>-<7-digit-hash> 
 # If on tag (e.g. 1.0.0)
 # - any commit is pushed as :<model>-<semver>
 # - any commit is pushed as :<model>-latest
 # - any commit is pushed as :<model>
 git_hash=
 pr=
-local_repo=${LOCAL_REPO?Variable LOCAL_REPO is required}
 remote_repo=${REMOTE_REPO?Variable REMOTE_REPO is required}
 model_name=${MODEL_NAME?Variable MODEL_NAME is required}
+original_model_name=$model_name
 docker_username=${DOCKER_USERNAME?Variable DOCKER_USERNAME is required}
 docker_password=${DOCKER_PASSWORD?Variable DOCKER_PASSWORD is required}
 
@@ -42,6 +42,8 @@ function init() {
     pr="$GIT_PULL_REQUEST"
   fi
 
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+  docker buildx create --use
   echo "$docker_password" | docker login -u "$docker_username" --password-stdin
 }
 
@@ -52,23 +54,27 @@ function push_main() {
     # The ones that are always pushed
 
     tag="$remote_repo:$model_name-$git_hash"
-    docker tag "$local_repo" "$tag" && docker push "$tag"
+    docker buildx build --platform=linux/arm64,linux/amd64 \
+      --build-arg "MODEL_NAME=$original_model_name" \
+      --push \
+      --tag "$tag" .
   fi
 }
 
 function push_tag() {
   if [ ! -z "$GIT_TAG" ]; then
-    tag="$remote_repo:$model_name-$GIT_TAG"
-    echo "Tag & Push $tag"
-    docker tag "$local_repo" "$tag" && docker push "$tag"
-
-    tag="$remote_repo:$model_name-latest"
-    echo "Tag & Push $tag"
-    docker tag "$local_repo" "$tag" && docker push "$tag"
-
+    tag_git="$remote_repo:$model_name-$GIT_TAG"
+    tag_latest="$remote_repo:$model_name-latest"
     tag="$remote_repo:$model_name"
-    echo "Tag & Push $tag"
-    docker tag "$local_repo" "$tag" && docker push "$tag"
+
+    echo "Tag & Push $tag, $tag_latest, $tag_git"
+    docker buildx build --platform=linux/arm64,linux/amd64 \
+      --build-arg "MODEL_NAME=$original_model_name" \
+      --push \
+      --tag "$tag_git" \
+      --tag "$tag_latest" \
+      --tag "$tag" \
+      .
   fi
 }
 
