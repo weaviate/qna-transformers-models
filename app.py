1ad28a28b821aaba6a93687c8a1ca1e1b2a036b5
Stefan Bogdan <stefanbogdan180394@gmail.com>
2022-01-21 12:16:10 +0100

    Add uvicorn logger instead of prints, add startup even, fixes #19

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
+import os
+from logging import getLogger
 from fastapi import FastAPI, Response, status
 from qna import Qna, AnswersInput
 from meta import Meta
-import os
+
 
 app = FastAPI()
+qna : Qna
+meta_config : Meta
+logger = getLogger('uvicorn')
 
-cuda_env = os.getenv("ENABLE_CUDA")
-cuda_support=False
-cuda_core=""
-if cuda_env is not None and cuda_env == "true" or cuda_env == "1":
-    cuda_support=True
-    cuda_core = os.getenv("CUDA_CORE")
-    if cuda_core is None or cuda_core == "":
-        cuda_core = "cuda:0"
-    print("[INFO] cuda core set to {}".format(cuda_core))
-else:
-    print("[INFO] running on CPU")
 
-qna = Qna('./models/model', cuda_support, cuda_core)
-meta_config = Meta('./models/model')
+@app.on_event("startup")
+def startup_event():
+    global qna
+    global meta_config
+
+    cuda_env = os.getenv("ENABLE_CUDA")
+    cuda_support=False
+    cuda_core=""
+
+    if cuda_env is not None and cuda_env == "true" or cuda_env == "1":
+        cuda_support=True
+        cuda_core = os.getenv("CUDA_CORE")
+        if cuda_core is None or cuda_core == "":
+            cuda_core = "cuda:0"
+        logger.info(f"CUDA_CORE set to {cuda_core}")
+    else:
+        logger.info("Running on CPU")
+
+    qna = Qna('./models/model', cuda_support, cuda_core)
+    meta_config = Meta('./models/model')
 
 
 @app.get("/.well-known/live", response_class=Response)
@@ -26,21 +38,25 @@ meta_config = Meta('./models/model')
 def live_and_ready(response: Response):
     response.status_code = status.HTTP_204_NO_CONTENT
 
+
 @app.get("/meta")
 def meta():
     return meta_config.get()
+
 
 @app.post("/answers/")
 async def read_item(item: AnswersInput, response: Response):
     try:
         answer, certainty = await qna.do(item)
         return {
-                    "text": item.text,
-                    "question": item.question,
-                    "answer": answer,
-                    "certainty": certainty,
-                }
+            "text": item.text,
+            "question": item.question,
+            "answer": answer,
+            "certainty": certainty,
+        }
     except Exception as e:
+        logger.exception(
+            'Something went wrong while vectorizing data.'
+        )
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        print("[ERROR] " + str(e))
         return {"error": str(e)}
